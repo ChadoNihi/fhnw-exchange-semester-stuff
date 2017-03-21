830e9ae741fbef129a57ff0694726de4c516439d
ChadoNihi <chadoishere@gmail.com>
2017-03-21 21:51:44 +0200

    save

--- math/discr_sto/ba-spam-filter.py
+++ math/discr_sto/ba-spam-filter.py
@@ -1,10 +1,31 @@
-def is_spam(fl_name):
+class SpamFilter(object):
+    def __init__(self, arg):
+        pass
 
-    with open(fl_name, 'r') as f:
+    def is_spam(self, email):
+        words = [word for word in re.split('[.!?,:;]|\s', fl.read()) if len(word)>1 or word in ['I', 'i']]
+
+
+
+
+def is_spam(fl_name, threshold):
+
+    with open(fl_name, 'r') as fl:
         import re
 
-        words = re.split('[.]|\s', f.read())
+        words = [word for word in re.split('[.!?,:;]|\s', fl.read()) if len(word)>1 or word in ['I', 'i']]
 
+        spam_prob = 0
+        for word in words:
+            pass
+
+def train():
+    for value in variable:
+        pass
 
 if __name__ == "__main__":
-    pass
+    import os
+    filter = SpamFilter()
+
+
+    with open(fl_name, 'r') as fl:
