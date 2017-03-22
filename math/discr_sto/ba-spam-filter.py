b12f301064e0d0b563e1f37a29235c4f9575b4b4
ChadoNihi <chadoishere@gmail.com>
2017-03-22 14:52:25 +0200

    save

--- math/discr_sto/ba-spam-filter.py
+++ math/discr_sto/ba-spam-filter.py
@@ -1,9 +1,15 @@
 class SpamFilter(object):
-    def __init__(self, arg):
-        pass
+    def __init__(self):
+        self.train()
 
     def is_spam(self, email):
         words = [word for word in re.split('[.!?,:;]|\s', fl.read()) if len(word)>1 or word in ['I', 'i']]
+
+        for word in words:
+            pass
+
+    def train(self):
+        pass
 
 
 
@@ -24,8 +30,44 @@ def train():
         pass
 
 if __name__ == "__main__":
-    import os
+    from os import getcwd, listdir
+    from os.path import isfile, join
+
     filter = SpamFilter()
 
+    # testing on jams:
+    num_jams = 0
+    num_predicted_jams = 0
+    for dir_item in listdir(join(getcwd(), 'jam-test')):
+        try:
+            with open(dir_item, 'r') as jam_fl:
+                num_jams += 1
+                if not filter.is_spam(jam_fl.read()):
+                    num_predicted_jams += 1
+        except OSError:
+            print('Cannot open ', dir_item)
+            continue
 
-    with open(fl_name, 'r') as fl:
+    if num_jams == 0:
+        print('Found no test jams.')
+    else:
+        print('Accuracy on JAM tests: %.3f' % (num_predicted_jams / num_jams)*100)
+
+
+    # testing on spams:
+    num_spams = 0
+    num_predicted_spams = 0
+    for dir_item in listdir(join(getcwd(), 'spam-test')):
+        try:
+            with open(dir_item, 'r') as spam_fl:
+                num_spams += 1
+                if filter.is_spam(spam_fl.read()):
+                    num_predicted_spams += 1
+        except OSError:
+            print('Cannot open ', dir_item)
+            continue
+
+    if num_spams == 0:
+        print('Found no test spams.')
+    else:
+        print('Accuracy on SPAM tests: %.3f' % (num_predicted_spams / num_spams)*100)
