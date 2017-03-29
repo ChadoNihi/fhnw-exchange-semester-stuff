8c8b0eb989b82b6d09b419204ea54345d4a8384e
ChadoNihi <chadoishere@gmail.com>
2017-03-29 14:47:01 +0300

    save

--- ml/Assignment_1/solution.py
+++ ml/Assignment_1/solution.py
@@ -16,8 +16,21 @@ def load_data(need_printing = False):
 
     print('%d data entries has been read.\n' % len(rows))
 
-def fname(arg):
-    pass
+    return rows
+
+def task_2(rows):
+    lr = linear_model.LinearRegression(C = 1e12) #large C -> small lambda -> ~no regularization
+    regr.fit(diabetes_X_train, diabetes_y_train)
+
+    plt.scatter(diabetes_X_test, diabetes_y_test,  color='black')
+    plt.plot(diabetes_X_test, lr.predict(diabetes_X_test), color='blue', linewidth=3)
+
+    plt.xticks(())
+    plt.yticks(())
+
+    plt.show()
+
+# HELPER FUNCTIONS
 
 if __name__ == '__main__':
     from sys import argv
