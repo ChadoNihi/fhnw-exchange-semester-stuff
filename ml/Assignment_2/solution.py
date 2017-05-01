099b5a85f0a10fa175dee60e49d6c9d6c443205b
ChadoNihi <chadoishere@gmail.com>
2017-05-02 01:04:58 +0300

    save

--- ml/Assignment_2/solution.py
+++ ml/Assignment_2/solution.py
@@ -1,6 +1,7 @@
 from math import exp
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.optimize as opt
 
 def task_1(points_for_each_class):
     print('TASK 1\n')
@@ -15,19 +16,42 @@ def task_1(points_for_each_class):
     plt.show()
 
 def task_2(points_for_each_class):
-    pass
+    print('TASK 2\n')
 
 
 # LOGISTIC REG. FUNCTIONS
+# (following Stanford's ML MOOC (https://www.coursera.org/learn/machine-learning/home)
+#            and http://www.johnwittenauer.net/machine-learning-exercises-in-python-part-3/)
 
-def cost():
-    pass
+def cost(X, Y, T, l = 0):
+    reg = (l / 2 * len(X)) * np.sum(np.power(T[:,1:T.shape[1]], 2))
+    return np.sum( np.multiply(-Y, np.log(lr_h(X, T)))
+                    - np.multiply((1-Y), np.log(1 - lr_h(X, T))) ) / len(X) + reg
+
+def grad_step(T, X, Y, l):
+    n = T.ravel().shape[1]
+
+    grad = np.zeros(n)
+    err = lr_h(X, T) - Y
+
+    grad[0] = np.sum(np.multiply(err, X[:,0])) / len(X)
+    for i in range(1, n):
+        grad[i] = np.sum(np.multiply(err, X[:,i])) / len(X) + (l / len(X))*T[:,i]
+
+    return grad
+
+def h(X, T):
+    return np.dot(X, T)
 
 def lr_h(X, T):
     return sig(h(X,T))
 
-def h(X, T):
-    return np.dot(X, T)
+def optimise_T(X, Y, T, l):
+    return opt.fmin_tnc(func=cost, x0=T, fprime=grad_step, args=(X, Y, l))
+
+def predict(X, Y, T, l):
+    P = lr_h(X, optimise_T(X, Y, T, l)[0])
+    return [1 if x >= 0.5 else 0 for x in P]
 
 def sig(Z):
 	return 1 / (1 + np.exp(-Z))
