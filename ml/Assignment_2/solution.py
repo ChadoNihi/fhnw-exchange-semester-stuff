6316e1f9793873ef8fb2aa5c9fab333c0a2f72c6
ChadoNihi <chadoishere@gmail.com>
2017-05-05 01:11:54 +0300

    save

--- ml/Assignment_2/solution.py
+++ ml/Assignment_2/solution.py
@@ -18,11 +18,27 @@ def task_1(points_for_each_class):
 def task_2(points_for_each_class):
     print('TASK 2\n')
 
-    X = np.matrix(points_for_each_class[0] + points_for_each_class[1])
+    len_0 = len(points_for_each_class[0])
+    len_1 = len(points_for_each_class[1])
+
+    X = np.matrix(np.c_[np.ones(len_0+len_1), np.concatenate([points_for_each_class[0], points_for_each_class[1]])])
+    print(X)
     # 1 means 'belongs to class 0'
-    Y = np.concatenate([np.ones(len(points_for_each_class[0])), np.zeros(len(points_for_each_class[1]))])
+    Y = np.matrix(np.concatenate([np.ones(len_0), np.zeros(len_1)]))
+    T = np.matrix(np.zeros(len(points_for_each_class[0][0])+1))
+    print(type(X), type(T))
+
+    pred_Y = predict(X, T)
+
+    print('Predicted classification:')
+    print(pred_Y)
+    print('True classification:')
     print(Y)
-    #predictedY = predict(X, optimise_T(X, Y, T, l)[0])
+
+    print('\nAccuracy: %f' % calc_accuracy(pred_Y, Y))
+    print('Precision: %f' % calc_precision(pred_Y, Y))
+    print('Recall: %f' % calc_recall(pred_Y, Y))
+    print('F1 score: %f' % calc_F1(pred_Y, Y))
 
 
 # LOGISTIC REG. FUNCTIONS
@@ -47,7 +63,7 @@ def grad_step(T, X, Y, l=0):
     return grad
 
 def h(X, T):
-    return np.dot(X, T)
+    return np.dot(X, T.T)
 
 def lr_h(X, T):
     return sig(h(X,T))
@@ -63,6 +79,50 @@ def sig(Z):
 	return 1 / (1 + np.exp(-Z))
 
 # HELPER FUNCTIONS
+
+def calc_accuracy(pred_Y, Y):
+    m = len(pred_Y)
+    if m != len(Y): raise ValueError('Error: pred_Y and Y should be of the same length.')
+
+    n_right = 0
+    for i in range(m):
+        if pred_Y[i] == Y[i]: n_right += 1
+
+    return n_right / m
+
+def calc_F1(pred_Y, Y):
+    acc = calc_accuracy(pred_Y, Y)
+    rec = calc_recall(pred_Y, Y)
+
+    return 2*acc*rec / (acc+rec)
+
+def calc_precision(pred_Y, Y):
+    m = len(pred_Y)
+    if m != len(Y): raise ArgumentError('Error: pred_Y and Y should be of the same length.')
+
+    tp, fp = 0, 0
+    for i in range(m):
+        if pred_Y[i] == 1 and Y[i] == 1:
+            tp += 1
+        elif pred_Y[i] == 1 and Y[i] == 0:
+            fp += 1
+
+    return tp / (tp+fp)
+
+def calc_recall(pred_Y, Y):
+    m = len(pred_Y)
+    if m != len(Y): raise ArgumentError('Error: pred_Y and Y should be of the same length.')
+
+    tp, p = 0, 0
+    for i in range(m):
+        if pred_Y[i] == 1 and Y[i] == 1:
+            tp += 1
+        if Y[i] == 1:
+            p += 1
+
+    return tp / p
+
+
 
 def gen_data(means, sdevs, n_points_per_class):
     from random import gauss
