97f28d0e5585f37005884ec59836a04b539477e6
ChadoNihi <chadoishere@gmail.com>
2017-05-16 23:47:37 +0300

    start ML assignment 3

---
+++ ml/Assignment_3/solution.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.optimize as opt
+
+def task_1(points_for_each_class):
+    print('TASK 1\n')
+
+if __name__ == '__main__':
+    task_1()
