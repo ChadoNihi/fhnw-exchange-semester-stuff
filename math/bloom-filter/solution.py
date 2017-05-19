7ace98c07333da1d7730b4462e9b31ecba73cf0f
ChadoNihi <chadoishere@gmail.com>
2017-05-19 23:01:33 +0300

    scaffold bloom filter

---
+++ math/bloom-filter/solution.py
@@ -0,0 +1,12 @@
+class BloomFilter():
+    def __init__(self):
+        pass
+
+    def insert(self, str):
+        pass
+
+    def is_exist(self, str):
+        pass
+
+if __name__ == '__main__':
+    pass
