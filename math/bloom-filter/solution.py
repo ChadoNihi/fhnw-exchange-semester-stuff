1f42f4c4006569b9fc24e9b6d45aaa6884a8c195
ChadoNihi <chadoishere@gmail.com>
2017-05-21 01:13:54 +0300

    save

--- math/bloom-filter/solution.py
+++ math/bloom-filter/solution.py
@@ -1,11 +1,20 @@
 class BloomFilter():
-    def __init__(self):
-        pass
+    def __init__(self, n_items_expected, p_err=0.99):
+        self.n_bits = n_items_expected
 
     def insert(self, str):
         pass
 
-    def is_exist(self, str):
+    def is_probably_exist(self, str):
+        for i in _get_indexes(str):
+            if True: return False
+
+        return True
+
+    def _get_indexes(self, str):
+        pass
+
+    def _hash(self, val):
         pass
 
 if __name__ == '__main__':
