0796b86de0dd4f0b18b5030f8c768b81791d17ec
ChadoNihi <chadoishere@gmail.com>
2017-05-21 22:35:51 +0300

    save

--- math/bloom-filter/solution.py
+++ math/bloom-filter/solution.py
@@ -1,9 +1,24 @@
 class BloomFilter():
-    def __init__(self, n_items_expected, p_err=0.99):
-        self.n_bits = n_items_expected
+    def __init__(self, n_items_expected, p_err=0.01, print_stats = False):
+        from math import log, ceil
+
+        if p_err > 1 or p_err < 0:
+            raise ValueError()
+
+        if n_items_expected <= 0: n_items_expected = 1
+
+        # formulas from https://en.wikipedia.org/wiki/Bloom_filter#Optimal_number_of_hash_functions
+        self.n_bits = ceil(  (n_items_expected * log(p_err)) / (log(2)**2)  )
+        self.n_hashes = ceil(  -log(p_err) / log(2)  )
+
+        self.filter_array = 
+
+        print('Bloom Filter of %d bits and %d hashes created\n(given %d expected items and P(false positive)=%.3f)'
+                % (self.n_bits, self.n_hashes, n_items_expected, p_err))
 
     def insert(self, str):
-        pass
+        for i in _get_indexes(str):
+
 
     def is_probably_exist(self, str):
         for i in _get_indexes(str):
