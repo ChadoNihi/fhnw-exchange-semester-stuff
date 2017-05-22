e82b2c8af28087b3cbe726d4c51bbb558eef1c9e
ChadoNihi <chadoishere@gmail.com>
2017-05-22 23:54:08 +0300

    save

--- math/bloom-filter/solution.py
+++ math/bloom-filter/solution.py
@@ -11,18 +11,18 @@ class BloomFilter():
         self.n_bits = ceil(  (n_items_expected * log(p_err)) / (log(2)**2)  )
         self.n_hashes = ceil(  -log(p_err) / log(2)  )
 
-        self.filter_array = 
+        self.bit_array = MyBitArray(n_bits)
 
         print('Bloom Filter of %d bits and %d hashes created\n(given %d expected items and P(false positive)=%.3f)'
                 % (self.n_bits, self.n_hashes, n_items_expected, p_err))
 
     def insert(self, str):
-        for i in _get_indexes(str):
-
+        for i in self._get_indexes(str):
+            self.bit_array.set_bit(i)
 
     def is_probably_exist(self, str):
-        for i in _get_indexes(str):
-            if True: return False
+        for i in self._get_indexes(str):
+            if not self.bit_array.is_set(i): return False
 
         return True
 
@@ -31,6 +31,18 @@ class BloomFilter():
 
     def _hash(self, val):
         pass
+
+class MyBitArray():
+    from math import ceil, floor
+
+    def __init__(self, n_bits):
+        self._byte_arr = bytearray(ceil(n_bits/8))
+
+    def is_set(self, i):
+        return (self._byte_arr[floor(i/8)] & (1<<idx)) != 0
+
+    def set_bit(self, i):
+        self._byte_arr[floor(i/8)] |= (1 << (i%8))
 
 if __name__ == '__main__':
     pass
