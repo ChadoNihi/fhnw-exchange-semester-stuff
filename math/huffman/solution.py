da8b10c86786b56462667423cff773af264aec94
ChadoNihi <chadoishere@gmail.com>
2017-05-28 02:34:04 +0300

    save

--- math/huffman/solution.py
+++ math/huffman/solution.py
@@ -1,18 +1,25 @@
+
+
 def count_chars(str):
     chars = list(str)
     counts = dict()
     for ch in chars:
         counts[ch] = (1 if ch not in counts else counts[ch]+1)
 
+    counts['total_count'] = len(str)
+
     return counts
 
-def get_chars_freq(str):
-    counts = count_chars(str)
-    total = len(str)
-    return {ch: cnt / total for (ch, cnt) in counts}
+# def get_chars_freq(str):
+#     counts = count_chars(str)
+#     total = len(str)
+#     return {ch: cnt / total for (ch, cnt) in counts}
 
-def huff_code_from_freq(freq):
-    pass
+def huff_code_from_counts(counts):
+    import queue
+
+    pq = queue.PriorityQueue()
+
 
 if __name__ == '__main__':
     pass
