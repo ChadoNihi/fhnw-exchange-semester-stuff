27945ba9d8ddc522aa7fc32fb5402bac7cec98a1
ChadoNihi <chadoishere@gmail.com>
2017-06-07 00:47:19 +0300

    launchable

--- math/discr_sto/bloom-filter/solution.py
+++ math/discr_sto/bloom-filter/solution.py
@@ -1,40 +1,42 @@
+from math import ceil, floor, log
+try:
+    import mmh3
+except ImportError:
+    raise ImportError("Cannot import module 'mmh3'. Try to install it with 'pip install mmh3'")
+
 class BloomFilter():
     def __init__(self, n_items_expected, p_err=0.01, print_stats = False):
-        from math import log, ceil
-
         if p_err > 1 or p_err < 0:
             raise ValueError()
 
         if n_items_expected <= 0: n_items_expected = 1
 
         # formulas from https://en.wikipedia.org/wiki/Bloom_filter#Optimal_number_of_hash_functions
-        self.n_bits = ceil(  (n_items_expected * log(p_err)) / (log(2)**2)  )
+        self.n_bits = ceil(  -(n_items_expected * log(p_err)) / (log(2)**2)  )
         self.n_hashes = ceil(  -log(p_err) / log(2)  )
 
-        self.bit_array = MyBitArray(n_bits)
+        self.bit_array = MyBitArray(self.n_bits)
 
         print('Bloom Filter of %d bits and %d hashes created\n(given %d expected items and P(false positive)=%.3f)'
                 % (self.n_bits, self.n_hashes, n_items_expected, p_err))
 
-    def insert(self, str):
-        for i in self._get_indexes(str):
+    def insert(self, s):
+        for i in self._get_indexes(s):
             self.bit_array.set_bit(i)
 
-    def is_probably_exist(self, str):
-        for i in self._get_indexes(str):
+    def is_probably_exist(self, s):
+        for i in self._get_indexes(s):
             if not self.bit_array.is_set(i): return False
 
         return True
 
-    def _get_indexes(self, str):
-        pass
+    def _get_indexes(self, s):
+        return [self._hash(s, seed) % self.n_bits for seed in range(self.n_hashes)]
 
-    def _hash(self, val):
-        pass
+    def _hash(self, val, seed=None):
+        return mmh3.hash(val, seed) if seed else mmh3.hash(val)
 
 class MyBitArray():
-    from math import ceil, floor
-
     def __init__(self, n_bits):
         self._byte_arr = bytearray(ceil(n_bits/8))
 
